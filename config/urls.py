74aa842bff1f02192b69e69f844882e057348faf
Abolfazl Fallahkar <AbolfazlFallahkar8080@gmail.com>
2026-09-20 13:04:24 +0330

    Configure Django Ninja API routing matching Shop architecture

--- config/urls.py
+++ config/urls.py
@@ -2,30 +2,24 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView
-from drf_spectacular.views import SpectacularRedocView
-from drf_spectacular.views import SpectacularSwaggerView
-import debug_toolbar
-from product.views import PaymentVerifyView
+from ninja import NinjaAPI
+
+from authentication.api.urls import router as auth_router
+from product.api.urls import router as product_router
+
+api = NinjaAPI(docs_url="/docs/")
+
+api.add_router("/auth", auth_router)
+api.add_router("/product", product_router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('__debug__/', include(debug_toolbar.urls)),
-
-    # Schema
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(), name='redoc'),
-    path('api/schema/ui/', SpectacularSwaggerView.as_view(), name='schema_ui'),
-
-    # Authentication
-    path('api/auth/', include('authentication.urls')),
-
-    # Products
-    path('api/p/', include('product.urls')),
-    path('api/payment/callback/', PaymentVerifyView.as_view(), name='payment-verify'),
+    path('api/v1/', api.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
