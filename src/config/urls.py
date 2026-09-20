360ec3474d8ef97fab9fcf454f16c99ca20713d6
Abolfazl Fallahkar <AbolfazlFallahkar8080@gmail.com>
2026-09-20 13:23:31 +0330

    Configure Django Ninja API routing for catalog, promotions, authentication, and finance domains

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,12 +5,19 @@ from django.urls import path, include
 from ninja import NinjaAPI
 
 from authentication.api.urls import router as auth_router
-from product.api.urls import router as product_router
+from catalog.api.urls import router as catalog_router
+from promotions.api.urls import router as promotions_router
+from finance.api.urls import router as finance_router
 
 api = NinjaAPI(docs_url="/docs/")
 
+if not settings.DEBUG:
+    api.openapi_url = None
+
 api.add_router("/auth", auth_router)
-api.add_router("/product", product_router)
+api.add_router("/catalog", catalog_router)
+api.add_router("/promotions", promotions_router)
+api.add_router("/", finance_router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
